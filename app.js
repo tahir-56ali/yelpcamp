12468c7590e7ebc8a3c46c90bf68283415df682d
Tahir Ali <tahir.56ali@gmail.com>
2023-02-03 20:38:30 +0500

    port changes in app.js

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ store.on("error", function (e) {
 const sessionConfig = {
     store,
     name: 'yelpcamp',
-    secret: secret,
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -156,6 +156,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
 })
